91075ccf055ab58a37af4660babd344d7e797bda
Ilya Paripsa <ilya.paripsa@rea-group.com>
2015-09-18 07:59:28 +1000

    FlatMap Implementation

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -18,7 +18,9 @@ sealed trait Option[+A] {
     }
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    map(f) getOrElse None
+  }
 
   def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
 
